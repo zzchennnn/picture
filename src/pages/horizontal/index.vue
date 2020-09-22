1384c06378ba647ef1acaefd55548cffd6ef3bac
your name <your email>
2020-09-22 14:40:42 +0800

    my first commit

---
+++ src/pages/horizontal/index.vue
@@ -0,0 +1,13 @@
+<template>
+	<view class="content">横屏</view>
+</template>
+
+<script>
+	export default {
+
+	}
+</script>
+
+<style>
+	
+</style>
---
+++ src/pages/mine/index.vue
@@ -0,0 +1,13 @@
+<template>
+	<view class="content">我的</view>
+</template>
+
+<script>
+	export default {
+
+	}
+</script>
+
+<style>
+	
+</style>
